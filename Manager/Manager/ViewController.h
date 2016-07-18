36b4f2d0818fb4b85c7b24d2d3c2de578c4b97d3
David Livingstone <david@david.tl>
2016-07-18 13:32:06 -0700

    created project

---
+++ Manager/Manager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Manager
+//
+//  Created by David Livingstone on 7/18/16.
+//  Copyright © 2016 David Livingstone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
